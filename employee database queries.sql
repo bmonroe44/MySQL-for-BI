a1820261e921753fce9a44d25225c0c999c655da
Brian Monroe <brianmonroe77@gmail.com>
2019-01-20 01:11:05 -0600

    Update employee database queries.sql

--- employee database queries.sql
+++ employee database queries.sql
@@ -11,3 +11,9 @@ SELECT * FROM employees WHERE first_name = 'Kellie' OR first_name = 'Aruna';
 
 # Retrieve list of all female employees whose first name is either 'Kellie' or 'Aruna'
 SELECT * FROM employees WHERE gender = 'F' AND (first_name = 'Kellie' OR first_name = 'Aruna');
+
+# Select all individuals from employees table whose first name is either 'Denis' or 'Elvis'
+SELECT * FROM employees WHERE first_name IN ('Denis', 'Elvis');
+
+# Extract all employee records from employees table aside from 'John', 'Mark' and 'Jacob'
+SELECT * FROM employees WHERE first_name NOT IN ('John', 'Mark','Jacob');
