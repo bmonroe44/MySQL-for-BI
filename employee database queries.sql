4f6295beb0815983098ac75bb22100f03c947877
Brian Monroe <brianmonroe77@gmail.com>
2019-02-07 22:57:47 -0600

    Update employee database queries.sql
    Co-Authored-By: Brian Monroe <b_monroe44@yahoo.com>

--- employee database queries.sql
+++ employee database queries.sql
@@ -209,4 +209,136 @@ SELECT e.gender, COUNT(dm.emp_no) AS number_of_managers FROM employees e
 SELECT * FROM dept_manager
 	WHERE emp_no IN (SELECT emp_no FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1995-01-01');
 
+# Select all information for all employees whose job title is "Assistant Engineer'
+SELECT * FROM employees e WHERE EXISTS (SELECT * FROM titles t WHERE t.emp_no = e.emp_no AND title = 'Assistant Engineer');
 
+SELECT 
+    A.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A 
+UNION SELECT 
+    B.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no > 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS B;
+
+# Create emp_manager table and fill using subsets created using subqueries
+DROP TABLE IF EXISTS emp_manager;
+CREATE TABLE emp_manager (emp_no INT(11) NOT NULL, dept_no CHAR(4) NULL, manager_no INT(11) NOT NULL);
+
+INSERT INTO emp_manager SELECT
+U.*
+FROM
+	(SELECT 
+    A.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A 
+UNION SELECT 
+    B.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no > 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS B UNION SELECT
+    C.*
+FROM 
+	(SELECT
+		e.emp_no AS employee_ID, 
+			MIN(de.dept_no) AS department_code,
+            (SELECT
+					emp_no
+                FROM 
+					dept_manager
+				WHERE 
+					emp_no = 110039) AS manager_ID
+	FROM employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+		e.emp_no = 110022
+	GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS C UNION SELECT
+    D.*
+FROM 
+	(SELECT
+		e.emp_no AS employee_ID, 
+			MIN(de.dept_no) AS department_code,
+            (SELECT
+					emp_no
+                FROM 
+					dept_manager
+				WHERE 
+					emp_no = 110022) AS manager_ID
+	FROM employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+		e.emp_no = 110039
+	GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS D) AS U;
+    SELECT * FROM emp_manager;
+
+# Create View that extracts the average salary of all managers in the database
+CREATE OR REPLACE VIEW v_manager_avg_salary AS
+    SELECT 
+        ROUND(AVG(salary),2)
+    FROM
+        salaries s
+            JOIN
+        dept_manager m ON s.emp_no = m.emp_no;
