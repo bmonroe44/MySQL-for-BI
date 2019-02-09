da4c0563f2c8caee6b6c4b7636cd5daae5b19282
Brian Monroe <brianmonroe77@gmail.com>
2019-02-09 00:09:36 -0600

    Update employee database queries.sql
    Co-Authored-By: Brian Monroe <b_monroe44@yahoo.com>

--- employee database queries.sql
+++ employee database queries.sql
@@ -342,3 +342,159 @@ CREATE OR REPLACE VIEW v_manager_avg_salary AS
         salaries s
             JOIN
         dept_manager m ON s.emp_no = m.emp_no;
+
+# Stored Routines exercises 
+DROP PROCEDURE IF EXISTS select_employees;
+
+DELIMITER $$
+CREATE PROCEDURE select_employees()
+BEGIN
+			SELECT * FROM employees
+            LIMIT 1000;
+END$$
+
+DELIMITER ;
+
+CALL employees.select_employees();
+
+# Create procedure that provides average salary of all employees
+DELIMITER $$
+CREATE PROCEDURE average_salary()
+BEGIN
+		SELECT ROUND(AVG(salary)) FROM salaries;
+END$$
+
+DELIMITER ;
+
+CALL average_salary();
+
+DROP PROCEDURE select_employees;
+
+DELIMITER $$
+CREATE PROCEDURE emp_salary(IN p_emp_no INTEGER)
+BEGIN
+	SELECT 
+		e.first_name, e.last_name, s.salary, s.from_date, s.to_date
+	FROM
+		employees e
+			JOIN
+		salaries s ON e.emp_no = s.emp_no
+	WHERE 
+		e.emp_no = p_emp_no;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE emp_avg_salary(IN p_emp_no INTEGER)
+BEGIN
+	SELECT 
+		e.first_name, e.last_name, AVG(s.salary)
+	FROM
+		employees e
+			JOIN
+		salaries s ON e.emp_no = s.emp_no
+	WHERE 
+		e.emp_no = p_emp_no;
+END$$
+
+DELIMITER ;
+
+CALL emp_avg_salary(11300);
+
+DELIMITER $$
+CREATE PROCEDURE emp_avg_salary_out(in p_emp_no INTEGER, out p_avg_salary DECIMAL(10,2))
+BEGIN
+SELECT
+	AVG(s.salary)
+INTO p_avg_salary FROM
+	employees e
+		JOIN 
+	salaries s ON e.emp_no = s.emp_no
+WHERE
+	e.emp_no = p_emp_no;
+END$$
+
+DELIMITER ;   
+
+# Create procedure called 'emp_info that uses as parameters first and last name of an individual and returns employee number
+DELIMITER $$
+CREATE PROCEDURE emp_info(in p_first_name CHAR(255), in p_last_name CHAR(255), out p_emp_no INTEGER)
+BEGIN
+SELECT
+	e.emp_no
+INTO p_emp_no FROM
+	employees e
+WHERE 
+	e.first_name = p_first_name
+			AND e.last_name = p_last_name;
+END$$
+
+DELIMITER ;
+
+SET @v_avg_salary = 0;
+CALL employees.emp_avg_salary_out(11300, @v_avg_salry);
+SELECT @v_avg_salry;
+
+SET @v_emp_no = 0;
+CALL employees.emp_info('Aruna', 'Journel', @v_emp_no);
+SELECT @v_emp_no;
+
+DELIMITER $$
+CREATE FUNCTION f_emp_avg_salary(p_emp_no INTEGER) RETURNS DECIMAL(10,2)
+BEGIN
+
+DECLARE v_avg_salary DECIMAL(10,2);
+
+SELECT
+	AVG(s.salary)
+INTO v_avg_salary FROM
+	employees e
+		JOIN
+	salaries s ON e.emp_no = s.emp_no
+WHERE 
+	e.emp_no = p_emp_no;
+    
+RETURN v_avg_salary;
+END $$
+
+DELIMITER ;
+
+SELECT f_emp_avg_salary(11300);
+
+DELIMITER $$
+CREATE FUNCTION emp_info(p_first_name VARCHAR(255), p_last_name VARCHAR(255)) RETURNS DECIMAL(10,2)
+BEGIN
+
+DECLARE v_max_from_date DATE;
+
+DECLARE v_salary DECIMAL(10,2);
+
+SELECT
+	MAX(from_date)
+INTO v_max_from_date FROM
+	employees e
+		JOIN
+	salaries s ON e.emp_no = s.emp_no
+WHERE 
+	e.first_name = p_first_name
+		AND e.last_name = p_last_name;
+
+SELECT
+	s.salary
+INTO v_salary FROM
+	employees e
+		JOIN
+	salaries s ON e.emp_no = s.emp_no
+WHERE 
+	e.first_name = p_first_name
+		AND e.last_name = p_last_name
+        AND s.from_date = v_max_from_date;
+    
+RETURN v_salary;
+END $$
+
+DELIMITER ;
+
+SELECT emp_info('Aruna', 'Journel');
+	
