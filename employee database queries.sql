07dc357f56c363bdf4abc7c35e1042f6f7d8f8de
Brian Monroe <brianmonroe77@gmail.com>
2019-02-10 23:14:15 -0600

    Update employee database queries.sql
    Co-Authored-By: Brian Monroe <b_monroe44@yahoo.com>

--- employee database queries.sql
+++ employee database queries.sql
@@ -497,4 +497,102 @@ END $$
 DELIMITER ;
 
 SELECT emp_info('Aruna', 'Journel');
-	
+
+/* Advanced SQL Topics */
+/* Triggers */
+COMMIT;
+
+# BEFORE INSERT
+DELIMITER $$
+
+CREATE TRIGGER before_salaries_insert
+BEFORE INSERT ON salaries
+FOR EACH ROW
+BEGIN 
+	IF NEW.salary < 0 THEN 
+		SET NEW.salary = 0; 
+	END IF; 
+END $$
+
+DELIMITER ;
+
+SELECT * FROM salaries WHERE emp_no = '10001';
+
+INSERT INTO salaries VALUES ('10001', -92891, '2010-06-22', '9999-01-01');
+
+# BEFORE UPDATE
+DELIMITER $$
+
+CREATE TRIGGER trig_upd_salary
+BEFORE UPDATE ON salaries
+FOR EACH ROW
+BEGIN 
+	IF NEW.salary < 0 THEN 
+		SET NEW.salary = OLD.salary; 
+	END IF; 
+END $$
+
+DELIMITER ;
+
+UPDATE salaries 
+SET 
+    salary = 98765
+WHERE
+    emp_no = '10001'
+        AND from_date = '2010-06-22';
+        
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    emp_no = '10001'
+        AND from_date = '2010-06-22';
+        
+UPDATE salaries 
+SET 
+    salary = - 50000
+WHERE
+    emp_no = '10001'
+        AND from_date = '2010-06-22';
+        
+SELECT SYSDATE();
+
+SELECT DATE_FORMAT(SYSDATE(), '%Y-%m-%d') as today;
+
+DELIMITER $$
+
+CREATE TRIGGER trig_ins_dept_mng
+AFTER INSERT ON dept_manager
+FOR EACH ROW
+BEGIN
+	DECLARE v_curr_salary int;
+    
+    SELECT 
+		MAX(salary)
+	INTO v_curr_salary FROM
+		salaries
+	WHERE
+		emp_no = NEW.emp_no;
+
+	IF v_curr_salary IS NOT NULL THEN
+		UPDATE salaries 
+		SET 
+			to_date = SYSDATE()
+		WHERE
+			emp_no = NEW.emp_no and to_date = NEW.to_date;
+
+		INSERT INTO salaries 
+			VALUES (NEW.emp_no, v_curr_salary + 20000, NEW.from_date, NEW.to_date);
+    END IF;
+END $$
+
+DELIMITER ;
+
+INSERT INTO dept_manager VALUES ('111534', 'd009', date_format(sysdate(), '%Y-%m-%d'), '9999-01-01');
+
+SELECT * FROM dept_manager WHERE emp_no = 111534;
+
+SELECT * FROM salaries WHERE emp_no = 111534;
+
+ROLLBACK;
