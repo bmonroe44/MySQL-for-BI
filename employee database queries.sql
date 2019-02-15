0ccde8d2dd7765614d314014c56036749917abbc
Brian Monroe <brianmonroe77@gmail.com>
2019-02-14 20:38:35 -0600

    Update employee database queries.sql
    Co-Authored-By: Brian Monroe <b_monroe44@yahoo.com>

--- employee database queries.sql
+++ employee database queries.sql
@@ -627,3 +627,82 @@ CREATE INDEX i_hire_date ON employees(hire_date);
 # Drop i_hire_date index
 ALTER TABLE employees
 DROP INDEX i_hire_date;
+
+# Create index on salry column of salaries table
+SELECT * FROM salaries WHERE salary > 89000;
+CREATE INDEX i_salary ON salaries(salary);
+
+/* The CASE statement */
+SELECT emp_no, first_name, last_name,
+	CASE 
+		WHEN gender = 'M' THEN 'Male'
+        ELSE 'Female'
+	End AS gender
+FROM employees;
+
+SELECT emp_no, first_name, last_name, 
+	CASE gender
+		WHEN 'M' THEN 'Male' 
+        ELSE 'Female' 
+	END AS gender 
+FROM employees;
+
+SELECT emp_no, first_name, last_name,
+	IF(gender = 'M', 'Male', 'Female') AS gender
+FROM employees;
+
+SELECT dm.emp_no, e.first_name, e.last_name,
+	MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    CASE
+		WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'Salary was raised by more than $30,000'
+        WHEN MAX(s.salary) - MIN(s.salary) BETWEEN 20000 AND 30000 THEN 
+									'Salary was raised by more than $20,000, but less than $30,000'
+		ELSE 'Salary was raised by less than $20,000'
+	END AS salary_increase
+FROM dept_manager dm
+	JOIN employees e ON e.emp_no = dm.emp_no
+    JOIN salaries s ON s.emp_no = dm.emp_no
+GROUP BY s.emp_no;
+
+# CASE statement exercise #1
+SELECT e.emp_no, e.first_name, e.last_name, 
+	CASE WHEN dm.emp_no IS NOT NULL THEN 'Manager'
+    ELSE 'Employee'
+    END AS is_manager
+FROM employees e
+	LEFT JOIN dept_manager dm ON dm.emp_no = e.emp_no
+WHERE e.emp_no > 109990;
+
+# CASE statement exercise #2
+# Solution 1, exercise 2
+SELECT dm.emp_no, e.first_name, e.last_name,
+	MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    CASE
+		WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'Salary was raised by more than $30,000'
+        ELSE 'Salary was not raised by more than $30,000'
+	END AS salary_raise
+FROM dept_manager dm
+	JOIN employees e ON e.emp_no = dm.emp_no
+    JOIN salaries s ON s.emp_no = dm.emp_no
+GROUP BY s.emp_no;
+
+# Solution 2, exercise 2
+SELECT dm.emp_no, e.first_name, e.last_name,
+	MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    IF(MAX(s.salary) - MIN(s.salary) > 30000, 'Salary was raised by more than $30,000', 
+								'Salary was not raised by more than $30,000') AS salary_increase
+FROM dept_manager dm
+	JOIN employees e ON e.emp_no = dm.emp_no
+    JOIN salaries s ON s.emp_no = dm.emp_no
+GROUP BY s.emp_no;
+
+# CASE statement exercise 3
+SELECT e.emp_no, e.first_name, e.last_name,
+	CASE 
+		WHEN MAX(de.to_date) > SYSDATE() THEN 'Is still employed'
+        ELSE 'No longer employed'
+	END AS current_employee
+FROM employees e
+	JOIN dept_emp de ON de.emp_no = e.emp_no
+GROUP BY de.emp_no 
+LIMIT 100;
