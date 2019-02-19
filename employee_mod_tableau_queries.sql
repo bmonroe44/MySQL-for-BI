a494c6dbe8f44fd34e6eb66942fb86c3addb2097
Brian Monroe <brianmonroe77@gmail.com>
2019-02-18 21:02:23 -0600

    Update employee_mod_tableau_queries.sql
    Co-Authored-By: Brian Monroe <b_monroe44@yahoo.com>

--- employee_mod_tableau_queries.sql
+++ employee_mod_tableau_queries.sql
@@ -38,3 +38,50 @@ FROM
 		JOIN
 	t_employees ee ON dm.emp_no = ee.emp_no
 ORDER BY dm.emp_no, calendar_year;
+
+/* Task 3 */
+/* Compare the average salary of female versus male employees in the entire company until year 2002,
+and add a filter allowing you to see that per each department */
+SELECT 
+	e.gender,
+    d.dept_name,
+    ROUND(AVG(s.salary), 2) AS salary,
+    YEAR(s.from_date) AS calendar_year
+FROM 
+	t_salaries s
+		JOIN
+	t_employees e ON s.emp_no = e.emp_no
+		JOIN
+	t_dept_emp de ON e.emp_no = de.emp_no
+		JOIN
+	t_departments d ON de.dept_no = d.dept_no
+GROUP BY d.dept_no, e.gender, calendar_year
+HAVING calendar_year <= 2002
+ORDER BY d.dept_no;
+
+/* Task 4 */
+/* Create an SQL stored procedure that will allow you to obtain the average male and female salary per department within
+	a certain salary range. Let this range be defined by two values the user can insert when calling the procedure.
+    Visualize the obtained result-set in Tableau as a double bar chart */
+DELIMITER $$
+CREATE PROCEDURE filter_salary (IN p_min_salary FLOAT, IN p_max_salary FLOAT)
+BEGIN
+SELECT
+	e.gender, d.dept_name, AVG(s.salary) AS avg_salary
+FROM
+	t_salaries s
+		JOIN
+	t_employees e ON s.emp_no = e.emp_no
+		JOIN
+	t_dept_emp de ON de.emp_no = e.emp_no
+		JOIN 
+	t_departments d ON d.dept_no = de.dept_no
+    WHERE s.salary BETWEEN p_min_salary AND p_max_salary
+GROUP BY d.dept_no, e.gender;
+END$$
+
+DELIMITER ;
+
+CALL filter_salary(50000, 90000);
+
+
